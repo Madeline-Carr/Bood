2299e360e296cb38c1b2f8a161c0cd10582a59c4
Madeline Carr <madelinencarr@Gmail.com>
2019-04-13 23:13:08 -0400

    Fixed Numbers in showBood

--- script.js
+++ script.js
@@ -47,15 +47,15 @@ var bood = document.getElementById("bood");
 var boodWeight = 5;
 //bood.style.backgroundColor = "red";
 function showBood() {
-  if (boodWeight > 9) {
+  if (boodWeight > 6) {
     bood.src = "Images/skellyPug.jpg";
-  } else if (boodWeight > 8) {
+  } else if (boodWeight > 5) {
     bood.src = "Images/fatPug.jpg";
   } else if (boodWeight > 4) {
     bood.src = "Images/normalPug.jpg";
-  } else if (boodWeight > 2) {
+  } else if (boodWeight > 3) {
     bood.src = "Images/skinnyPug.jpg";
-  } else if (boodWeight > 1) {
+  } else if (boodWeight > 2) {
     bood.src = "Images/skellyPug.jpg";
   }
 
