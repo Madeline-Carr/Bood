5bc2c25cc4cf9234dd796ffaa9c987f4453b90b7
Madeline Carr <madelinencarr@Gmail.com>
2019-04-13 23:11:51 -0400

    Separated and Fixed showBood Function

--- script.js
+++ script.js
@@ -43,45 +43,49 @@ testObject.eats();
 
 */
 
-
-var bood = document.getElementById('bood');
+var bood = document.getElementById("bood");
 var boodWeight = 5;
-bood.style.backgroundColor = "red";
-function feedBood(){
-  boodWeight ++;
-  if (boodWeight>5 && boodWeight<7) {
-    bood.src="Images/fatPug.jpg"
-  } else if (boodWeight<1){
-    bood.src="Images/skellyPug.jpg"
-  } else if (boodWeight<2){
-    bood.src="Images/skinnyPug.jpg"
-  } else if (boodWeight==5){
-    bood.src="Images/normalPug.jpg"
-  } else if (boodWeight=>7){
-    bood.src="Images/skellyPug.jpg"
-  } else {
-    bood.src="Images/normalPug.jpg"
+//bood.style.backgroundColor = "red";
+function showBood() {
+  if (boodWeight > 9) {
+    bood.src = "Images/skellyPug.jpg";
+  } else if (boodWeight > 8) {
+    bood.src = "Images/fatPug.jpg";
+  } else if (boodWeight > 4) {
+    bood.src = "Images/normalPug.jpg";
+  } else if (boodWeight > 2) {
+    bood.src = "Images/skinnyPug.jpg";
+  } else if (boodWeight > 1) {
+    bood.src = "Images/skellyPug.jpg";
   }
-  console.log(boodWeight);
-};
 
-function starveBood(){
-  boodWeight --;
-  if (boodWeight>5 && boodWeight<9) {
-    bood.src="Images/fatPug.jpg"
-  } else if (boodWeight<1){
-    bood.src="Images/skellyPug.jpg"
-  } else if (boodWeight<3){
-    bood.src="Images/skinnyPug.jpg"
-  } else if (boodWeight===5){
-    bood.src="Images/normalPug.jpg"
-  } else if (boodWeight=>9){
-    bood.src="Images/skellyPug.jpg"
+  /*
+  if (boodWeight > 5 && boodWeight < 7) {
+    bood.src = "Images/fatPug.jpg";
+  } else if (boodWeight < 1) {
+    bood.src = "Images/skellyPug.jpg";
+  } else if (boodWeight < 2) {
+    bood.src = "Images/skinnyPug.jpg";
+  } else if (boodWeight == 5) {
+    bood.src = "Images/normalPug.jpg";
+  } else if (boodWeight => 7) {
+    bood.src = "Images/skellyPug.jpg";
   } else {
-    bood.src="Images/normalPug.jpg"
+    bood.src = "Images/normalPug.jpg";
   }
+  */
+}
+function feedBood() {
+  boodWeight++;
+  showBood();
   console.log(boodWeight);
-};
+}
+
+function starveBood() {
+  boodWeight--;
+  showBood();
+  console.log(boodWeight);
+}
 
 /*
 if (boodWeight==0) {
