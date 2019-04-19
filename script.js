8a46ac52c2fad4c81e02c26aea4f6de1afbb34fa
Madeline Carr <madelinencarr@Gmail.com>
2019-04-18 23:31:45 -0400

    Fix name and remove unnecessary id

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //Variables
-let boodElement = document.getElementById("boodElement");
+let boodPhoto = document.getElementById("boodPhoto");
 
 //Objects
 let bood = {
   weight: 5,
   showBood: function() {
     if (this.weight > 7) {
-      boodElement.src = "Images/skellyPug.jpg";
+      boodPhoto.src = "Images/skellyPug.jpg";
     } else if (this.weight > 5) {
-      boodElement.src = "Images/fatPug.jpg";
+      boodPhoto.src = "Images/fatPug.jpg";
     } else if (this.weight > 4) {
-      boodElement.src = "Images/normalPug.jpg";
+      boodPhoto.src = "Images/normalPug.jpg";
     } else if (this.weight > 3) {
-      boodElement.src = "Images/skinnyPug.jpg";
+      boodPhoto.src = "Images/skinnyPug.jpg";
     } else if (this.weight > 1) {
-      boodElement.src = "Images/skellyPug.jpg";
+      boodPhoto.src = "Images/skellyPug.jpg";
     }
   },
   feedBood: function() {
